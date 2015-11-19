a74c5c6b23a5b6bf250d9ace0c9d9c37396c2afa
Wes Kelly <weskelly@mac.com>
2015-11-18 20:49:13 -0500

    City inherits from intersection. RoadsScholar has floydWarshall implemented, but does not store predecessors yet.

--- src/roadsscholar/RoadsScholar.java
+++ src/roadsscholar/RoadsScholar.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package roadsscholar;
 
-/**
- *
- * @author wes
- */
 public class RoadsScholar
 {
     /**
@@ -20,4 +10,72 @@ public class RoadsScholar
         // TODO code application logic here
     }
     
+    public float[][] solve(String fileName)
+    {
+        Road roads[] = null;// parseInputFile(fileName);
+        
+        float adjacencyMatrix[][] = makeWeightedAdjacencyMatrix(roads);
+        
+        float best[][] = floydWarshall(adjacencyMatrix);
+        
+        return best;
+    }
+    
+    private float[][] floydWarshall(float adjacencyMatrix[][])
+    {
+        int n = adjacencyMatrix.length;
+        float best[][][] = new float[n][n][n];
+        int path[][] = new int[n][n];
+        
+        best[0] = adjacencyMatrix;
+        int numVertices = adjacencyMatrix.length;
+        
+        for (int k = 0; k < numVertices; k++)
+        {
+            for (int u = 0; u < numVertices; u++)
+            {
+                for (int v = 0; v < numVertices; v++)
+                {
+                    best[k][u][v] = best[k - 1][u][v];
+                    
+                    if ((best[k - 1][u][k] + best[k - 1][k][v]) < best[k][u][v])
+                    {
+                        best[k][u][v] = best[k - 1][u][k] + best[k - 1][k][v];
+                        path[u][v] = k;
+                    }
+                }
+            }
+        }
+        
+        return best[n];
+    }
+    
+    private float[][] makeWeightedAdjacencyMatrix(Road roads[])
+    {
+        // defaults to being filled with 0's by the Java language spec
+        float adjacencyMatrix[][] = new float[roads.length][roads.length];
+        
+        for (Road road : roads)
+        {
+            adjacencyMatrix[road.start()][road.end()] = road.length();
+            adjacencyMatrix[road.end()][road.start()]++;
+        }
+        
+        printMatrix(adjacencyMatrix);
+        
+        return adjacencyMatrix;
+    }
+    
+    private void printMatrix(float matrix[][])
+    {
+        System.out.println("----");
+        for (float j[] : matrix)
+        {
+            for (float i : j)
+            {
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+    }
 }
